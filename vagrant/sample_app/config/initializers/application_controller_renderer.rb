535359bde9c82b8731d2147db2f2775ad977acea
NTM-M <ntm.m9898@gmail.com>
2019-05-13 20:02:44 +0900

    [finisht]3-8

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '567c6b9eb831ee50931d8793da1f0e14f981b3d2da205db799d3b9efb0a50f841ae59982da7fbca281bb6aea9497b6c5a131bc9b2f08cd0690d9accbd4022cc5'
