f5853e2383b66629c33aff8a86562c0ee2422969
NTM-M <ntm.m9898@gmail.com>
2019-05-13 18:56:26 +0900

    [start]3-7

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -3,26 +3,6 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
-  def create #privateの下に書かない
-
-  	#ストロングバラメーターを使用
-  	post = Post.new(post_params)
-
-    #データベースへ保存する
-  	post.save
-
-    #トップ画面へリダイレクト
-  	redirect_to '/top'
-  end
-
-  def index
-  	@posts = Post.all
-  end
-
-  def show
-  	@post = Post.find(params[:id])
-  end
-
   def create
   	post = Post.new(post_params)
 
@@ -33,8 +13,19 @@ class PostsController < ApplicationController
   	redirect_to post_path(post.id)
   end
 
-  private
+  def index
+  	@posts = Post.all
+  end
 
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  private
   def post_params
   	params.require(:post).permit(:title, :body)
   end
